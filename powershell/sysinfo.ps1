a8eff4dc66944e5edc3425fb771736dcde75cd8d
uc-botheaj <88747352+uc-botheaj@users.noreply.github.com>
2021-10-05 18:07:44 -0400

    Update sysinfo.ps1

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,7 +1,15 @@
-﻿function getIP {
-    (Get-NetIPAddress).IPv4Address | Select-String "192*"    
+
+#$Hello = "Hello, PowerShell!" 
+#Write-Host($Hello) 
+function getIP {
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
 }
 
+write-host(getIP)
 $IP = getIP
-write-host("This machine's IP is $IP")
-write-host("This machine's IP is {0}" -f $IP) 
+$Date = ""
+$Body = "This machine's IP is $IP. User is $env:username. Hostname is $. PowerShell Version . Today's date is $Date"
+
+write-host($Body) 
+
+#Send-MailMessage -To "aj.bothe@gmail.com" -From "aj.bothe@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $Body -SmtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)  
